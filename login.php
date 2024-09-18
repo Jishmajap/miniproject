1abdc0980acb389869d60f1bc70898776fe7d502
Jishmajap <134680076+Jishmajap@users.noreply.github.com>
2024-09-18 18:39:26 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="log1.css">
+</head>
+<body >
+<ul>
+            <li><a href="login.php">Login</a></li>
+            <li ><a  href="#about">About </a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="vehicle.php">Home</a></li>
+
+ 
+</ul> 
+<div id="container" class="flex-container" style="background-color: #f2f2f2;">
+       <h1 style="text-alig:center">Login</h1>
+        <form>
+             
+        </form>
+</div>   
+</body>
+</html>
---
+++ vehicle.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+
+        <title>vehicle repair</title>
+
+</head>
+<body >
+
+
+
+
+
+<ul>
+            <li><a href="login.php">Login</a></li>
+            <li ><a  href="#about">About </a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="vehicle.php">Home</a></li>
+
+ 
+</ul>
+   
+<div id="align">
+
+    <div class="color" >
+        <h1 class="color">Service at your
+        <br> finger tip.</h1>
+    </div>
+                <h5 class="col">  Lorem ipsum dolor sit amet, consectetur<br>
+                    adipiscing elit, sed do eiusmod tempor<br>
+                    incididunt ut labore et dolore</h5>
+    <form >
+      <a href="acess.php">  <input type="button"  value="Find Now" name="Find Now" class="button" ></a>
+    </form>
+</div>
+
+</body>
+</html>
