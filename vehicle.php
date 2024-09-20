6cbca1d46544336335b4ac0620acbfe91e4b3ab8
jishmajap <jishmajish2004@gmail.com>
2024-09-20 21:08:34 +0530

    about section

--- vehicle.php
+++ vehicle.php
@@ -12,9 +12,7 @@
 </head>
 <body >
 
-
-
-
+<section class="background">
 
 <ul>
             <li><a href="login.php">Login</a></li>
@@ -38,6 +36,28 @@
       <a href="acess.php">  <input type="button"  value="Find Now" name="Find Now" class="button" ></a>
     </form>
 </div>
+</section>
+
+<section class="about">
+    <div class="abcontent" >
+      <h2 class="head">Who We Are</h2>
+      <p>
+        We are a passionate team dedicated to providing innovative solutions in the field of technology.<br>
+        Our mission is to empower individuals and organizations with cutting-edge tools that improve productivity,</br>
+        foster creativity, and drive progress.
+      </p>
+
+      
+
+      <h2>Our Journey</h2>
+      <p>
+        Since our inception in 2020, we have grown from a small startup into a vibrant community of tech enthusiasts
+        and professionals. Our commitment to excellence and customer satisfaction has driven our success,
+        and we continue to push the boundaries of what's possible.
+      </p>
+    </div>
+  </section>
+
 
 </body>
 </html>
